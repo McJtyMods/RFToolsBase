22e540d9b9753b8faf2dfe9ce7279238d33a1140
McJty <mcjty1@gmail.com>
2019-06-16 20:23:41 +0200

    Initial commit

---
+++ src/main/java/mcjty/rftoolsbase/RFToolsBase.java
@@ -0,0 +1,42 @@
+package mcjty.rftoolsbase;
+
+import mcjty.lib.base.ModBase;
+import mcjty.lib.setup.IProxy;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+
+@Mod(RFToolsBase.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RFToolsBase implements ModBase {
+
+    public static final String MODID = "rftoolsbase";
+
+    //@SuppressWarnings("PublicField")
+    //public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+    //@SuppressWarnings("PublicField")
+    //public static ModSetup setup = new ModSetup();
+
+    public RFToolsBase() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+    }
+
+    public void init(final FMLCommonSetupEvent event) {
+        //setup.init(event);
+        //proxy.init(event);
+    }
+
+
+    @Override
+    public String getModId() {
+        return RFToolsBase.MODID;
+    }
+
+    @Override
+    public void openManual(PlayerEntity entityPlayer, int i, String s) {
+        // @todo
+    }
+}
