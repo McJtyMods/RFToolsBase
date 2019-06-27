96a31ab59630bf2369fd897221f4fdbffdcad671
McJty <mcjty1@gmail.com>
2019-06-27 15:10:28 +0200

    Some fixes but baked models aren't working yet

--- src/main/java/mcjty/rftoolsbase/setup/ClientRegistration.java
+++ src/main/java/mcjty/rftoolsbase/setup/ClientRegistration.java
@@ -1,12 +1,13 @@
 package mcjty.rftoolsbase.setup;
 
 
+import mcjty.rftoolsbase.RFToolsBase;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = RFToolsBase.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientRegistration {
 
     @SubscribeEvent
