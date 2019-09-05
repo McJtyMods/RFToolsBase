3bec4810fb13e47d8ccdb1b4cd7f3906b90b9ac5
McJty <slto15aGH10>
2019-09-05 19:09:14 +0200

    Porting work. Preparations for moving stuff to RFTools Utility and RFTools Builder

---
+++ src/main/java/mcjty/rftoolsbase/api/compat/JEIRecipeAcceptor.java
@@ -0,0 +1,10 @@
+package mcjty.rftoolsbase.api.compat;
+
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+public interface JEIRecipeAcceptor {
+
+    void setGridContents(List<ItemStack> stacks);
+}
