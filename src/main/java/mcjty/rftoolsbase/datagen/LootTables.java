6498bd59c3cc338719081d78d3cc7918c00a49c9
McJty <mcjty1@gmail.com>
2019-09-17 05:09:41 +0200

    More work on recipes, loot tables and advancements

--- src/main/java/mcjty/rftoolsbase/datagen/LootTables.java
+++ src/main/java/mcjty/rftoolsbase/datagen/LootTables.java
@@ -1,59 +1,23 @@
 package mcjty.rftoolsbase.datagen;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import mcjty.lib.datagen.BaseLootTableProvider;
 import mcjty.rftoolsbase.blocks.ModBlocks;
-import net.minecraft.block.Block;
+import mcjty.rftoolsbase.items.ModItems;
+import mcjty.rftoolsbase.modules.infuser.MachineInfuserSetup;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DirectoryCache;
-import net.minecraft.data.IDataProvider;
-import net.minecraft.data.LootTableProvider;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-
-public class LootTables extends LootTableProvider {
-
-    private static final Logger LOGGER = LogManager.getLogger();
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-
-    private final Map<Block, LootTable.Builder> lootTables = new HashMap<>();
-    private final DataGenerator generator;
+public class LootTables extends BaseLootTableProvider {
 
     public LootTables(DataGenerator dataGeneratorIn) {
         super(dataGeneratorIn);
-        this.generator = dataGeneratorIn;
     }
 
     @Override
-    public void act(DirectoryCache cache) {
-        LootPool.Builder builder = LootPool.builder().rolls(ConstantRange.of(1));
-        LootTable.Builder tableBuilder = LootTable.builder().addLootPool(builder);
-        lootTables.put(ModBlocks.DIMENSIONAL_SHARD_OVERWORLD, tableBuilder);
-
-        Map<ResourceLocation, LootTable> tables = new HashMap<>();
-        for (Map.Entry<Block, LootTable.Builder> entry : lootTables.entrySet()) {
-            tables.put(entry.getKey().getLootTable(), entry.getValue().setParameterSet(LootParameterSets.BLOCK).build());
-        }
-        writeTables(cache, tables);
-    }
-
-    private void writeTables(DirectoryCache cache, Map<ResourceLocation, LootTable> tables) {
-        Path outputFolder = this.generator.getOutputFolder();
-        tables.forEach((key, lootTable) -> {
-            Path path = outputFolder.resolve("data/" + key.getNamespace() + "/loot_tables/" + key.getPath() + ".json");
-            try {
-                IDataProvider.save(GSON, cache, LootTableManager.toJson(lootTable), path);
-            } catch (IOException e) {
-                LOGGER.error("Couldn't write loot table {}", path, e);
-            }
-        });
+    protected void addTables() {
+        lootTables.put(MachineInfuserSetup.MACHINE_INFUSER, createStandardTable("infuser", MachineInfuserSetup.MACHINE_INFUSER));
+        lootTables.put(ModBlocks.DIMENSIONAL_SHARD_OVERWORLD, createSilkTouchTable("dimshard", ModBlocks.DIMENSIONAL_SHARD_OVERWORLD, ModItems.DIMENSIONALSHARD, 4.0f, 5.0f));
+        lootTables.put(ModBlocks.DIMENSIONAL_SHARD_END, createSilkTouchTable("dimshard", ModBlocks.DIMENSIONAL_SHARD_END, ModItems.DIMENSIONALSHARD, 4.0f, 5.0f));
+        lootTables.put(ModBlocks.DIMENSIONAL_SHARD_NETHER, createSilkTouchTable("dimshard", ModBlocks.DIMENSIONAL_SHARD_NETHER, ModItems.DIMENSIONALSHARD, 4.0f, 5.0f));
     }
 
     @Override
--- src/main/java/mcjty/rftoolsbase/datagen/Recipes.java
+++ src/main/java/mcjty/rftoolsbase/datagen/Recipes.java
@@ -1,8 +1,14 @@
 package mcjty.rftoolsbase.datagen;
 
+import mcjty.rftoolsbase.items.ModItems;
+import mcjty.rftoolsbase.modules.infuser.MachineInfuserSetup;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.Items;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
@@ -14,5 +20,61 @@ public class Recipes extends RecipeProvider {
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(ModItems.DIMENSIONALSHARD)
+                .patternLine("deg")
+                .patternLine("irG")
+                .patternLine("qcL")
+                .key('q', Items.QUARTZ)
+                .key('r', Items.REDSTONE)
+                .key('c', Items.PRISMARINE_SHARD)
+                .key('d', Items.DIAMOND)
+                .key('e', Items.EMERALD)
+                .key('g', Items.GOLD_INGOT)
+                .key('G', Items.GLOWSTONE_DUST)
+                .key('i', Items.IRON_INGOT)
+                .key('L', Items.GLASS)
+                .setGroup("rftools")
+                .addCriterion("gold", InventoryChangeTrigger.Instance.forItems(Items.GOLD_INGOT,
+                        Items.QUARTZ, Items.REDSTONE, Items.PRISMARINE_SHARD, Items.DIAMOND,
+                        Items.EMERALD, Items.GLOWSTONE_DUST, Items.IRON_INGOT, Items.GLASS))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.MACHINE_BASE)
+                .patternLine("ggg")
+                .patternLine("sss")
+                .key('g', Items.GOLD_NUGGET)
+                .key('s', Tags.Items.STONE)
+                .setGroup("rftools")
+                .addCriterion("gold", InventoryChangeTrigger.Instance.forItems(Items.GOLD_NUGGET))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.MACHINE_FRAME)
+                .patternLine("ili")
+                .patternLine("g g")
+                .patternLine("ili")
+                .key('g', Items.GOLD_NUGGET)
+                .key('i', Items.IRON_INGOT)
+                .key('l', Tags.Items.DYES_BLUE)
+                .setGroup("rftools")
+                .addCriterion("iron_ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT, Items.GOLD_NUGGET))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(MachineInfuserSetup.MACHINE_INFUSER)
+                .patternLine("srs")
+                .patternLine("dMd")
+                .patternLine("srs")
+                .key('r', Items.REDSTONE)
+                .key('s', ModItems.DIMENSIONALSHARD)
+                .key('d', Items.DIAMOND)
+                .key('M', ModItems.MACHINE_FRAME)
+                .setGroup("rftools")
+                .addCriterion("machine_frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_FRAME, ModItems.DIMENSIONALSHARD))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.SMARTWRENCH)
+                .patternLine("  i")
+                .patternLine(" l ")
+                .patternLine("l  ")
+                .key('i', Items.IRON_INGOT)
+                .key('l', Tags.Items.DYES_BLUE)
+                .setGroup("rftools")
+                .addCriterion("iron", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT))
+                .build(consumer);
     }
 }
