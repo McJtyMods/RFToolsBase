de5e2c247fc29e5a6ccb4e16e12231d7705d8195
mcjty <slto15aGH10>
2020-08-25 15:32:17 +0200

    Many cleanups

--- src/main/java/mcjty/rftoolsbase/RFToolsBase.java
+++ src/main/java/mcjty/rftoolsbase/RFToolsBase.java
@@ -1,6 +1,7 @@
 package mcjty.rftoolsbase;
 
 import mcjty.rftoolsbase.client.ClientInfo;
+import mcjty.rftoolsbase.setup.ClientSetup;
 import mcjty.rftoolsbase.setup.Config;
 import mcjty.rftoolsbase.setup.ModSetup;
 import mcjty.rftoolsbase.setup.Registration;
@@ -8,7 +9,6 @@ import mcjty.rftoolsbase.tools.TickOrderHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -33,7 +33,8 @@ public class RFToolsBase {
 
         Registration.register();
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener((FMLCommonSetupEvent e) -> setup.init(e));
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(setup::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::init);
         FMLJavaModLoadingContext.get().getModEventBus().addListener((FMLServerStartedEvent e) -> TickOrderHandler.clean());
     }
 }
--- src/main/java/mcjty/rftoolsbase/setup/ClientSetup.java
+++ src/main/java/mcjty/rftoolsbase/setup/ClientSetup.java
@@ -2,7 +2,6 @@ package mcjty.rftoolsbase.setup;
 
 
 import mcjty.lib.gui.GenericGuiContainer;
-import mcjty.rftoolsbase.RFToolsBase;
 import mcjty.rftoolsbase.client.RenderWorldLastEventHandler;
 import mcjty.rftoolsbase.keys.KeyBindings;
 import mcjty.rftoolsbase.keys.KeyInputHandler;
@@ -21,23 +20,18 @@ import mcjty.rftoolsbase.modules.tablet.items.TabletContainer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = RFToolsBase.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class ClientRegistration {
+public class ClientSetup {
 
-    @SubscribeEvent
     public static void init(FMLClientSetupEvent e) {
         DeferredWorkQueue.runLater(() -> {
             GenericGuiContainer.register(MachineInfuserSetup.CONTAINER_MACHINE_INFUSER.get(), GuiMachineInfuser::new);
-            ScreenManager.registerFactory(CraftingSetup.CONTAINER_CRAFTING_CARD.get(), ClientRegistration::createCraftingCardGui);
-            ScreenManager.registerFactory(FilterSetup.CONTAINER_FILTER_MODULE.get(), ClientRegistration::createFilterModuleGui);
-            ScreenManager.registerFactory(TabletSetup.CONTAINER_TABLET.get(), ClientRegistration::createTabletGui);
+            ScreenManager.registerFactory(CraftingSetup.CONTAINER_CRAFTING_CARD.get(), ClientSetup::createCraftingCardGui);
+            ScreenManager.registerFactory(FilterSetup.CONTAINER_FILTER_MODULE.get(), ClientSetup::createFilterModuleGui);
+            ScreenManager.registerFactory(TabletSetup.CONTAINER_TABLET.get(), ClientSetup::createTabletGui);
         });
         InformationScreenRenderer.register();
         MinecraftForge.EVENT_BUS.addListener(RenderWorldLastEventHandler::tick);
