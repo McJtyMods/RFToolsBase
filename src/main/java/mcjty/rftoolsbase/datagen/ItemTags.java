585add263c3a35ac8a31a31538c495030d1f4ec3
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-05-24 16:26:04 +0200

    Add machine_infuser_catalysts tag

--- src/main/java/mcjty/rftoolsbase/datagen/ItemTags.java
+++ src/main/java/mcjty/rftoolsbase/datagen/ItemTags.java
@@ -1,5 +1,8 @@
 package mcjty.rftoolsbase.datagen;
 
+import mcjty.rftoolsbase.RFToolsBase;
+import mcjty.rftoolsbase.modules.infuser.MachineInfuserModule;
+import mcjty.rftoolsbase.modules.various.VariousModule;
 import mcjty.rftoolsbase.modules.worldgen.WorldGenModule;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
@@ -10,13 +13,15 @@ import net.minecraftforge.common.data.ForgeBlockTagsProvider;
 public class ItemTags extends ItemTagsProvider {
 
     public ItemTags(DataGenerator generator, ExistingFileHelper helper) {
-        super(generator, new ForgeBlockTagsProvider(generator, helper));
+        super(generator, new ForgeBlockTagsProvider(generator, helper), RFToolsBase.MODID, helper);
     }
 
     @Override
     protected void registerTags() {
         getOrCreateBuilder(Tags.Items.ORES)
                 .add(WorldGenModule.DIMENSIONAL_SHARD_END_ITEM.get(), WorldGenModule.DIMENSIONAL_SHARD_NETHER_ITEM.get(), WorldGenModule.DIMENSIONAL_SHARD_OVERWORLD_ITEM.get());
+        getOrCreateBuilder(MachineInfuserModule.MACHINE_INFUSER_CATALYSTS)
+                .add(VariousModule.DIMENSIONALSHARD.get());
     }
 
     @Override
--- src/main/java/mcjty/rftoolsbase/modules/infuser/MachineInfuserModule.java
+++ src/main/java/mcjty/rftoolsbase/modules/infuser/MachineInfuserModule.java
@@ -3,6 +3,7 @@ package mcjty.rftoolsbase.modules.infuser;
 import mcjty.lib.blocks.BaseBlock;
 import mcjty.lib.container.GenericContainer;
 import mcjty.lib.modules.IModule;
+import mcjty.rftoolsbase.RFToolsBase;
 import mcjty.rftoolsbase.modules.infuser.blocks.MachineInfuserTileEntity;
 import mcjty.rftoolsbase.modules.infuser.client.GuiMachineInfuser;
 import mcjty.rftoolsbase.setup.Config;
@@ -10,7 +11,10 @@ import mcjty.rftoolsbase.setup.Registration;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -23,6 +27,7 @@ public class MachineInfuserModule implements IModule {
     public static final RegistryObject<Item> MACHINE_INFUSER_ITEM = ITEMS.register("machine_infuser", () -> new BlockItem(MACHINE_INFUSER.get(), Registration.createStandardProperties()));
     public static final RegistryObject<TileEntityType<?>> TYPE_MACHINE_INFUSER = TILES.register("machine_infuser", () -> TileEntityType.Builder.create(MachineInfuserTileEntity::new, MACHINE_INFUSER.get()).build(null));
     public static final RegistryObject<ContainerType<GenericContainer>> CONTAINER_MACHINE_INFUSER = CONTAINERS.register("machine_infuser", GenericContainer::createContainerType);
+    public static final ITag.INamedTag<Item> MACHINE_INFUSER_CATALYSTS = ItemTags.createOptional(new ResourceLocation(RFToolsBase.MODID, "machine_infuser_catalysts"));
 
     @Override
     public void init(FMLCommonSetupEvent event) {
