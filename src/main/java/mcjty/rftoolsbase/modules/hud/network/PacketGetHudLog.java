4efaeace347b07b30a203c5b6abb8deccfa2319f
McJty <mcjty1@gmail.com>
2021-11-18 06:51:11 +0100

    Work on a better list command system. WIP

--- src/main/java/mcjty/rftoolsbase/modules/hud/network/PacketGetHudLog.java
+++ src/main/java/mcjty/rftoolsbase/modules/hud/network/PacketGetHudLog.java
@@ -1,60 +1,38 @@
 package mcjty.rftoolsbase.modules.hud.network;
 
-import mcjty.lib.network.TypedMapTools;
-import mcjty.lib.tileentity.GenericTileEntity;
+import mcjty.lib.network.AbstractPacketGetListFromServer;
 import mcjty.lib.typed.TypedMap;
-import mcjty.lib.varia.Logging;
 import mcjty.rftoolsbase.setup.RFToolsBaseMessages;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.network.NetworkDirection;
-import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 import java.util.List;
-import java.util.function.Supplier;
 
-public class PacketGetHudLog {
+public class PacketGetHudLog extends AbstractPacketGetListFromServer<String> {
 
     public static final String COMMAND_GETHUDLOG = "getHudLog";
 
-    protected BlockPos pos;
-    protected TypedMap params;
-
-    public PacketGetHudLog() {
-    }
-
     public PacketGetHudLog(PacketBuffer buf) {
-        pos = buf.readBlockPos();
-        params = TypedMapTools.readArguments(buf);
+        super(buf);
     }
 
     public PacketGetHudLog(BlockPos pos) {
-        this.pos = pos;
-        this.params = TypedMap.EMPTY;
+        super(pos, COMMAND_GETHUDLOG, TypedMap.EMPTY);
     }
 
-    public void toBytes(PacketBuffer buf) {
-        buf.writeBlockPos(pos);
-        TypedMapTools.writeArguments(buf, params);
+    @Override
+    protected SimpleChannel getChannel() {
+        return RFToolsBaseMessages.INSTANCE;
     }
 
-    public void handle(Supplier<NetworkEvent.Context> supplier) {
-        NetworkEvent.Context ctx = supplier.get();
-        ctx.enqueueWork(() -> {
-            World world = ctx.getSender().getCommandSenderWorld();
-            if (world.hasChunkAt(pos)) {
-                TileEntity te = world.getBlockEntity(pos);
-                if (te instanceof GenericTileEntity) {
-                    List<String > list = ((GenericTileEntity) te).executeServerCommandList(COMMAND_GETHUDLOG, ctx.getSender(), TypedMap.EMPTY, String.class);
-                    RFToolsBaseMessages.INSTANCE.sendTo(new PacketHudLogReady(pos, COMMAND_GETHUDLOG, list),
-                            ctx.getSender().connection.connection, NetworkDirection.PLAY_TO_CLIENT);
-                } else {
-                    Logging.log("Command " + COMMAND_GETHUDLOG + " was not handled!");
-                }
-            }
-        });
-        ctx.setPacketHandled(true);
+    @Override
+    protected Class<String> getType() {
+        return String.class;
+    }
+
+    @Override
+    protected Object createReturnPacket(List<String> list) {
+        return new PacketHudLogReady(pos, command, list);
     }
 }
--- src/main/java/mcjty/rftoolsbase/modules/hud/network/PacketHudLogReady.java
+++ src/main/java/mcjty/rftoolsbase/modules/hud/network/PacketHudLogReady.java
@@ -1,48 +1,28 @@
 package mcjty.rftoolsbase.modules.hud.network;
 
-import mcjty.lib.network.NetworkTools;
-import mcjty.lib.tileentity.GenericTileEntity;
+import mcjty.lib.network.AbstractPacketSendResultToClient;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fml.network.NetworkEvent;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 
-public class PacketHudLogReady {
-
-    private BlockPos pos;
-    private List<String> list;
-    private String command;
-
-    public PacketHudLogReady() {
-    }
+public class PacketHudLogReady extends AbstractPacketSendResultToClient<String> {
 
     public PacketHudLogReady(PacketBuffer buf) {
-        pos = buf.readBlockPos();
-        command = buf.readUtf(32767);
-        list = NetworkTools.readStringList(buf);
+        super(buf);
     }
 
     public PacketHudLogReady(BlockPos pos, String command, List<String> list) {
-        this.pos = pos;
-        this.command = command;
-        this.list = new ArrayList<>();
-        this.list.addAll(list);
+        super(pos, command, list);
     }
 
-    public void toBytes(PacketBuffer buf) {
-        buf.writeBlockPos(pos);
-        buf.writeUtf(command);
-        NetworkTools.writeStringList(buf, list);
+    @Override
+    protected String readElement(PacketBuffer buf) {
+        return buf.readUtf(32767);
     }
 
-    public void handle(Supplier<NetworkEvent.Context> supplier) {
-        NetworkEvent.Context ctx = supplier.get();
-        ctx.enqueueWork(() -> {
-            GenericTileEntity.executeClientCommandHelper(pos, command, list);
-        });
-        ctx.setPacketHandled(true);
+    @Override
+    protected void writeElement(PacketBuffer buf, String element) {
+        buf.writeUtf(element);
     }
 }
