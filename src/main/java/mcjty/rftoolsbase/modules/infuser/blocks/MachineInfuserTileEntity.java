6eb194cc35245f7d88ad9d358dbd8eabb325bce6
McJty <mcjty1@gmail.com>
2021-11-25 14:16:05 +0100

    More container type cleanups

--- src/main/java/mcjty/rftoolsbase/modules/infuser/blocks/MachineInfuserTileEntity.java
+++ src/main/java/mcjty/rftoolsbase/modules/infuser/blocks/MachineInfuserTileEntity.java
@@ -29,6 +29,7 @@ import net.minecraftforge.common.util.LazyOptional;
 import javax.annotation.Nonnull;
 import java.util.Optional;
 
+import static mcjty.lib.api.container.DefaultContainerProvider.container;
 import static mcjty.lib.builder.TooltipBuilder.*;
 import static mcjty.lib.container.SlotDefinition.specific;
 
@@ -50,9 +51,10 @@ public class MachineInfuserTileEntity extends GenericTileEntity implements ITick
 
     @Cap(type = CapType.CONTAINER)
     private final LazyOptional<INamedContainerProvider> screenHandler = LazyOptional.of(() -> new DefaultContainerProvider<GenericContainer>("Machine Infuser")
-            .containerSupplier(windowId -> new GenericContainer(MachineInfuserModule.CONTAINER_MACHINE_INFUSER, windowId, CONTAINER_FACTORY, this))
+            .containerSupplier(container(MachineInfuserModule.CONTAINER_MACHINE_INFUSER, CONTAINER_FACTORY,this))
             .itemHandler(() -> items)
-            .energyHandler(() -> energyStorage));
+            .energyHandler(() -> energyStorage)
+            .setupSync(this));
 
     @Cap(type = CapType.INFUSABLE)
     private final LazyOptional<IInfusable> infusableHandler = LazyOptional.of(() -> new DefaultInfusable(MachineInfuserTileEntity.this));
