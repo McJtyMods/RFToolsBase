f6b6e894ec19d65fecefe48d2994c738596378e9
Alex Emelyanov <trinadtsatyi@gmail.com>
2024-06-17 21:41:45 +0300

    refactoring rs checking method

--- src/main/java/mcjty/rftoolsbase/api/xnet/helper/DefaultChannelSettings.java
+++ src/main/java/mcjty/rftoolsbase/api/xnet/helper/DefaultChannelSettings.java
@@ -1,5 +1,6 @@
 package mcjty.rftoolsbase.api.xnet.helper;
 
+import mcjty.rftoolsbase.api.xnet.channels.IControllerContext;
 import mcjty.rftoolsbase.api.xnet.channels.RSMode;
 import mcjty.rftoolsbase.api.xnet.tiles.IConnectorTile;
 import net.minecraft.core.BlockPos;
@@ -7,6 +8,7 @@ import net.minecraft.world.level.Level;
 
 public class DefaultChannelSettings {
 
+    @Deprecated
     protected boolean checkRedstone(Level world, AbstractConnectorSettings settings, BlockPos extractorPos) {
         RSMode rsMode = settings.getRsMode();
         if (rsMode != RSMode.IGNORED) {
@@ -22,6 +24,31 @@ public class DefaultChannelSettings {
             }
         }
         return false;
+    }
+
+    /**
+     * Method to check redstone conditions
+     * @return true if connector should work, false otherwise
+     */
+    protected boolean checkRedstone(AbstractConnectorSettings settings, IConnectorTile connector, IControllerContext context) {
+        RSMode rsMode = settings.getRsMode();
+        switch (rsMode) {
+            case IGNORED -> {return true;}
+
+            case OFF -> {
+                //Match color return true if color has RS signal. RSMode.OFF means that we don't need signal to work
+                return (settings.getColorsMask() == 0 || !context.matchColor(settings.getColorsMask())) && connector.getPowerLevel() == 0;
+            }
+            case PULSE -> {
+                int prevPulse = settings.getPrevPulse();
+                settings.setPrevPulse(connector.getPulseCounter());
+                return prevPulse != connector.getPulseCounter();
+            }
+            default -> {
+                return context.matchColor(settings.getColorsMask()) && connector.getPowerLevel() != 0;
+            }
+        }
+
     }
 
 }
