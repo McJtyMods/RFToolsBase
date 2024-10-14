71d989683b4fd6a12df3de5691d207c4ee360e0d
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-10-14 18:54:06 +0200

    More porting

--- src/main/java/mcjty/rftoolsbase/api/dimension/IDimensionInformation.java
+++ src/main/java/mcjty/rftoolsbase/api/dimension/IDimensionInformation.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public interface IDimensionInformation {
 
     /**
-     * Get the owner of this dimension.
+     * Get the ownerUUID of this dimension.
      */
     UUID getOwner();
 
