aec856c04f60c744c70b04203c810b1d35fbfc65
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-10-15 18:27:13 +0200

    Porting and fighting with codecs

--- src/main/java/mcjty/rftoolsbase/api/xnet/channels/RSMode.java
+++ src/main/java/mcjty/rftoolsbase/api/xnet/channels/RSMode.java
@@ -1,8 +1,22 @@
 package mcjty.rftoolsbase.api.xnet.channels;
 
-public enum RSMode {
+import com.mojang.serialization.Codec;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.util.StringRepresentable;
+import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
+
+public enum RSMode implements StringRepresentable {
     IGNORED,
     OFF,
     ON,
-    PULSE
+    PULSE;
+
+    public static final Codec<RSMode> CODEC = StringRepresentable.fromEnum(RSMode::values);
+    public static final StreamCodec<FriendlyByteBuf, RSMode> STREAM_CODEC = NeoForgeStreamCodecs.enumCodec(RSMode.class);
+
+    @Override
+    public String getSerializedName() {
+        return name();
+    }
 }
--- src/main/java/mcjty/rftoolsbase/api/xnet/helper/AbstractConnectorSettings.java
+++ src/main/java/mcjty/rftoolsbase/api/xnet/helper/AbstractConnectorSettings.java
@@ -2,17 +2,22 @@ package mcjty.rftoolsbase.api.xnet.helper;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import mcjty.lib.varia.OrientationTools;
 import mcjty.rftoolsbase.api.xnet.channels.Color;
 import mcjty.rftoolsbase.api.xnet.channels.IConnectorSettings;
 import mcjty.rftoolsbase.api.xnet.channels.RSMode;
 import mcjty.rftoolsbase.api.xnet.gui.IEditorGui;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 public abstract class AbstractConnectorSettings implements IConnectorSettings {
@@ -21,18 +26,36 @@ public abstract class AbstractConnectorSettings implements IConnectorSettings {
     public static final String TAG_COLOR = "color";
     public static final String TAG_FACING = "facing";
 
-    private RSMode rsMode = RSMode.IGNORED;
-    private int prevPulse = 0;
-    private Color[] colors = new Color[]{Color.OFF, Color.OFF, Color.OFF, Color.OFF};
+    protected BaseSettings settings = new BaseSettings(RSMode.IGNORED, Color.OFF, Color.OFF, Color.OFF, Color.OFF, null);
     private int colorsMask = 0;
+    private int prevPulse = 0;
 
     // Cache for advanced mode
     protected boolean advanced = false;
-
     @Nonnull
     private final Direction side;
-    @Nullable
-    private Direction facingOverride = null; // Only available on advanced connectors
+
+    public record BaseSettings(RSMode rsMode, Color color0, Color color1, Color color2, Color color3, Direction facingOverride) {
+
+        public static final Codec<BaseSettings> CODEC = RecordCodecBuilder.create(instance ->
+                instance.group(
+                        RSMode.CODEC.fieldOf("rsMode").forGetter(BaseSettings::rsMode),
+                        Color.CODEC.fieldOf("color0").forGetter(BaseSettings::color0),
+                        Color.CODEC.fieldOf("color1").forGetter(BaseSettings::color1),
+                        Color.CODEC.fieldOf("color2").forGetter(BaseSettings::color2),
+                        Color.CODEC.fieldOf("color3").forGetter(BaseSettings::color3),
+                        Direction.CODEC.optionalFieldOf("facingOverride").forGetter(s -> Optional.ofNullable(s.facingOverride()))
+                ).apply(instance, (rsMode, c0, c1, c2, c3, direction) -> new BaseSettings(rsMode, c0, c1, c2, c3, direction.orElse(null))));
+
+        public static final StreamCodec<FriendlyByteBuf, BaseSettings> STREAM_CODEC = StreamCodec.composite(
+                RSMode.STREAM_CODEC, BaseSettings::rsMode,
+                Color.STREAM_CODEC, BaseSettings::color0,
+                Color.STREAM_CODEC, BaseSettings::color1,
+                Color.STREAM_CODEC, BaseSettings::color2,
+                Color.STREAM_CODEC, BaseSettings::color3,
+                ByteBufCodecs.optional(Direction.STREAM_CODEC), s -> Optional.ofNullable(s.facingOverride()),
+                (rsMode, c0, c1, c2, c3, direction) -> new BaseSettings(rsMode, c0, c1, c2, c3, direction.orElse(null)));
+    }
 
     public AbstractConnectorSettings(@Nonnull Direction side) {
         this.side = side;
@@ -45,11 +68,11 @@ public abstract class AbstractConnectorSettings implements IConnectorSettings {
 
     @Nonnull
     public Direction getFacing() {
-        return facingOverride == null ? side : facingOverride;
+        return settings.facingOverride == null ? side : settings.facingOverride;
     }
 
     public RSMode getRsMode() {
-        return rsMode;
+        return settings.rsMode;
     }
 
     public int getPrevPulse() {
@@ -60,16 +83,19 @@ public abstract class AbstractConnectorSettings implements IConnectorSettings {
         this.prevPulse = prevPulse;
     }
 
-    public Color[] getColors() {
-        return colors;
-    }
-
     private void calculateColorsMask() {
         colorsMask = 0;
-        for (Color color : colors) {
-            if (color != null && color != Color.OFF) {
-                colorsMask |= 1 << color.ordinal();
-            }
+        if (settings.color0 != Color.OFF) {
+            colorsMask |= 1 << settings.color0.ordinal();
+        }
+        if (settings.color1 != Color.OFF) {
+            colorsMask |= 1 << settings.color1.ordinal();
+        }
+        if (settings.color2 != Color.OFF) {
+            colorsMask |= 1 << settings.color2.ordinal();
+        }
+        if (settings.color3 != Color.OFF) {
+            colorsMask |= 1 << settings.color3.ordinal();
         }
     }
 
@@ -79,34 +105,40 @@ public abstract class AbstractConnectorSettings implements IConnectorSettings {
 
     @Override
     public void update(Map<String, Object> data) {
+        RSMode rsMode;
         if (data.containsKey(TAG_RS)) {
             rsMode = RSMode.valueOf(((String) data.get(TAG_RS)).toUpperCase());
         } else {
             rsMode = RSMode.IGNORED;
         }
+        Color colors0;
+        Color colors1;
+        Color colors2;
+        Color colors3;
         if (data.containsKey(TAG_COLOR + "0")) {
-            colors[0] = Color.colorByValue((Integer) data.get(TAG_COLOR + "0"));
+            colors0 = Color.colorByValue((Integer) data.get(TAG_COLOR + "0"));
         } else {
-            colors[0] = Color.OFF;
+            colors0 = Color.OFF;
         }
         if (data.containsKey(TAG_COLOR + "1")) {
-            colors[1] = Color.colorByValue((Integer) data.get(TAG_COLOR + "1"));
+            colors1 = Color.colorByValue((Integer) data.get(TAG_COLOR + "1"));
         } else {
-            colors[1] = Color.OFF;
+            colors1 = Color.OFF;
         }
         if (data.containsKey(TAG_COLOR + "2")) {
-            colors[2] = Color.colorByValue((Integer) data.get(TAG_COLOR + "2"));
+            colors2 = Color.colorByValue((Integer) data.get(TAG_COLOR + "2"));
         } else {
-            colors[2] = Color.OFF;
+            colors2 = Color.OFF;
         }
         if (data.containsKey(TAG_COLOR + "3")) {
-            colors[3] = Color.colorByValue((Integer) data.get(TAG_COLOR + "3"));
+            colors3 = Color.colorByValue((Integer) data.get(TAG_COLOR + "3"));
         } else {
-            colors[3] = Color.OFF;
+            colors3 = Color.OFF;
         }
-        calculateColorsMask();
         String facing = (String) data.get(TAG_FACING);
-        facingOverride = facing == null ? null : Direction.byName(facing.toLowerCase());
+        Direction facingOverride = facing == null ? null : Direction.byName(facing.toLowerCase());
+        settings = new BaseSettings(rsMode, colors0, colors1, colors2, colors3, facingOverride);
+        calculateColorsMask();
     }
 
     protected static <T extends Enum<T>> void setEnumSafe(JsonObject object, String tag, T value) {
@@ -154,65 +186,66 @@ public abstract class AbstractConnectorSettings implements IConnectorSettings {
     }
 
     protected void writeToJsonInternal(JsonObject object) {
-        setEnumSafe(object, "rsmode", rsMode);
-        setEnumSafe(object, "color0", colors[0]);
-        setEnumSafe(object, "color1", colors[1]);
-        setEnumSafe(object, "color2", colors[2]);
-        setEnumSafe(object, "color3", colors[3]);
+        setEnumSafe(object, "rsmode", settings.rsMode);
+        setEnumSafe(object, "color0", settings.color0);
+        setEnumSafe(object, "color1", settings.color1);
+        setEnumSafe(object, "color2", settings.color2);
+        setEnumSafe(object, "color3", settings.color3);
         setEnumSafe(object, "side", side);   // Informative, isn't used to load
-        setEnumSafe(object, "facingoverride", facingOverride);
+        setEnumSafe(object, "facingoverride", settings.facingOverride);
         object.add("advancedneeded", new JsonPrimitive(false));
     }
 
     protected void readFromJsonInternal(JsonObject object) {
-        rsMode = getEnumSafe(object, "rsmode", BaseStringTranslators::getRSMode);
-        colors[0] = getEnumSafe(object, "color0", BaseStringTranslators::getColor);
-        colors[1] = getEnumSafe(object, "color1", BaseStringTranslators::getColor);
-        colors[2] = getEnumSafe(object, "color2", BaseStringTranslators::getColor);
-        colors[3] = getEnumSafe(object, "color3", BaseStringTranslators::getColor);
-        facingOverride = getEnumSafe(object, "facingoverride", s -> Direction.byName(s.toLowerCase()));
+        RSMode rsMode = getEnumSafe(object, "rsmode", BaseStringTranslators::getRSMode);
+        Color color0 = getEnumSafe(object, "color0", BaseStringTranslators::getColor);
+        Color color1 = getEnumSafe(object, "color1", BaseStringTranslators::getColor);
+        Color color2 = getEnumSafe(object, "color2", BaseStringTranslators::getColor);
+        Color color3 = getEnumSafe(object, "color3", BaseStringTranslators::getColor);
+        Direction facingOverride = getEnumSafe(object, "facingoverride", s -> Direction.byName(s.toLowerCase()));
+        settings = new BaseSettings(rsMode, color0, color1, color2, color3, facingOverride);
     }
 
     @Override
     public void readFromNBT(CompoundTag tag) {
-        rsMode = RSMode.values()[tag.getByte("rsMode")];
-        prevPulse = tag.getInt("prevPulse");
-        colors[0] = Color.values()[tag.getByte("color0")];
-        colors[1] = Color.values()[tag.getByte("color1")];
-        colors[2] = Color.values()[tag.getByte("color2")];
-        colors[3] = Color.values()[tag.getByte("color3")];
-        calculateColorsMask();
+        RSMode rsMode = RSMode.values()[tag.getByte("rsMode")];
+        Color color0 = Color.values()[tag.getByte("color0")];
+        Color color1 = Color.values()[tag.getByte("color1")];
+        Color color2 = Color.values()[tag.getByte("color2")];
+        Color color3 = Color.values()[tag.getByte("color3")];
+        Direction facingOverride = settings.facingOverride;
         if (tag.contains("facingOverride")) {
             facingOverride = OrientationTools.DIRECTION_VALUES[tag.getByte("facingOverride")];
         }
+        settings = new BaseSettings(rsMode, color0, color1, color2, color3, facingOverride);
+        calculateColorsMask();
     }
 
     @Override
     public void writeToNBT(CompoundTag tag) {
-        tag.putByte("rsMode", (byte) rsMode.ordinal());
-        tag.putInt("prevPulse", prevPulse);
-        tag.putByte("color0", (byte) colors[0].ordinal());
-        tag.putByte("color1", (byte) colors[1].ordinal());
-        tag.putByte("color2", (byte) colors[2].ordinal());
-        tag.putByte("color3", (byte) colors[3].ordinal());
-        if (facingOverride != null) {
-            tag.putByte("facingOverride", (byte) facingOverride.ordinal());
+        tag.putByte("rsMode", (byte) settings.rsMode.ordinal());
+        tag.putByte("color0", (byte) settings.color0.ordinal());
+        tag.putByte("color1", (byte) settings.color1.ordinal());
+        tag.putByte("color2", (byte) settings.color2.ordinal());
+        tag.putByte("color3", (byte) settings.color3.ordinal());
+        if (settings.facingOverride != null) {
+            tag.putByte("facingOverride", (byte) settings.facingOverride.ordinal());
         }
     }
 
     protected IEditorGui sideGui(IEditorGui gui) {
-        return gui.choices(TAG_FACING, "Side from which to operate", facingOverride == null ? side : facingOverride, OrientationTools.DIRECTION_VALUES);
+        return gui.choices(TAG_FACING, "Side from which to operate", settings.facingOverride == null ? side : settings.facingOverride, OrientationTools.DIRECTION_VALUES);
     }
 
     protected IEditorGui colorsGui(IEditorGui gui) {
         return gui
-                .colors(TAG_COLOR + "0", "Enable on color", colors[0].getColor(), Color.COLORS)
-                .colors(TAG_COLOR + "1", "Enable on color", colors[1].getColor(), Color.COLORS)
-                .colors(TAG_COLOR + "2", "Enable on color", colors[2].getColor(), Color.COLORS)
-                .colors(TAG_COLOR + "3", "Enable on color", colors[3].getColor(), Color.COLORS);
+                .colors(TAG_COLOR + "0", "Enable on color", settings.color0.getColor(), Color.COLORS)
+                .colors(TAG_COLOR + "1", "Enable on color", settings.color1.getColor(), Color.COLORS)
+                .colors(TAG_COLOR + "2", "Enable on color", settings.color2.getColor(), Color.COLORS)
+                .colors(TAG_COLOR + "3", "Enable on color", settings.color3.getColor(), Color.COLORS);
     }
 
     protected IEditorGui redstoneGui(IEditorGui gui) {
-        return gui.redstoneMode(TAG_RS, rsMode);
+        return gui.redstoneMode(TAG_RS, settings.rsMode);
     }
 }
