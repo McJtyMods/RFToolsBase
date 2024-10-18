9967b2f2ab82dd86b826e73c3ee41847ce9aa432
McJty <mcjty1@gmail.com>
2024-10-18 07:37:51 +0200

    More porting stuff

--- src/main/java/mcjty/rftoolsbase/api/xnet/keys/SidedPos.java
+++ src/main/java/mcjty/rftoolsbase/api/xnet/keys/SidedPos.java
@@ -3,6 +3,8 @@ package mcjty.rftoolsbase.api.xnet.keys;
 import mcjty.lib.varia.BlockPosTools;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 
 public record SidedPos(BlockPos pos, Direction side) implements Comparable<SidedPos> {
 
@@ -17,4 +19,10 @@ public record SidedPos(BlockPos pos, Direction side) implements Comparable<Sided
         if (result == 0) result = side.compareTo(o.side);
         return result;
     }
+
+    public static final StreamCodec<FriendlyByteBuf, SidedPos> STREAM_CODEC = StreamCodec.composite(
+            BlockPos.STREAM_CODEC, SidedPos::pos,
+            Direction.STREAM_CODEC, SidedPos::side,
+            SidedPos::new
+    );
 }
