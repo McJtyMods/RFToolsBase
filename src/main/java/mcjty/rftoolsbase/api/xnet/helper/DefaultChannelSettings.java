5e31612991ca732064186d5b0984e191180b6232
McJty <mcjty1@gmail.com>
2024-10-21 04:58:54 +0200

    Merged changes from 1.20 branch to 1.21_neo

--- src/main/java/mcjty/rftoolsbase/api/xnet/helper/DefaultChannelSettings.java
+++ src/main/java/mcjty/rftoolsbase/api/xnet/helper/DefaultChannelSettings.java
@@ -1,5 +1,6 @@
 package mcjty.rftoolsbase.api.xnet.helper;
 
+import mcjty.rftoolsbase.api.xnet.channels.IControllerContext;
 import mcjty.rftoolsbase.api.xnet.channels.RSMode;
 import mcjty.rftoolsbase.api.xnet.tiles.IConnectorTile;
 import net.minecraft.core.BlockPos;
@@ -7,6 +8,7 @@ import net.minecraft.world.level.Level;
 
 public class DefaultChannelSettings {
 
+    @Deprecated
     protected boolean checkRedstone(Level world, AbstractConnectorSettings settings, BlockPos extractorPos) {
         RSMode rsMode = settings.getRsMode();
         if (rsMode != RSMode.IGNORED) {
@@ -22,6 +24,27 @@ public class DefaultChannelSettings {
             }
         }
         return false;
+    }
+
+    /**
+     * Method to check redstone conditions
+     * @return true if connector should work, false otherwise
+     */
+    protected boolean checkRedstone(AbstractConnectorSettings settings, IConnectorTile connector, IControllerContext context) {
+        RSMode rsMode = settings.getRsMode();
+        boolean redstoneCondition;
+        switch (rsMode) {
+            case IGNORED -> {redstoneCondition = true;}
+            case OFF -> redstoneCondition = connector.getPowerLevel() == 0;
+            case PULSE -> {
+                int prevPulse = settings.getPrevPulse();
+                settings.setPrevPulse(connector.getPulseCounter());
+                redstoneCondition = prevPulse != connector.getPulseCounter();
+            }
+            default -> redstoneCondition = connector.getPowerLevel() != 0;
+        }
+
+        return context.matchColor(settings.getColorsMask()) && redstoneCondition;
     }
 
 }
