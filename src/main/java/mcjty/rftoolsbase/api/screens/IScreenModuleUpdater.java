22a5bcb854a41973c328a7d54665293bf198a8d6
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2025-07-04 20:16:00 +0200

    You never guess.... PORTING!!!

--- src/main/java/mcjty/rftoolsbase/api/screens/IScreenModuleUpdater.java
+++ src/main/java/mcjty/rftoolsbase/api/screens/IScreenModuleUpdater.java
@@ -1,7 +1,7 @@
 package mcjty.rftoolsbase.api.screens;
 
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 /**
@@ -12,15 +12,15 @@ import net.minecraft.world.level.Level;
 public interface IScreenModuleUpdater {
 
     /**
-     * Return a copy of the input tagCommpound with possible modifications
+     * Return a copy of the input module with possible modifications
      * if you want this. This method is guaranteed to be called right
      * after any interaction with the screen (mouse click) and on the server
-     * side. Return null if nothing has to be changed
-     * @param tagCompound
+     * side. Return ItemStack.EMPTY if nothing has to be changed
+     * @param module
      * @param world
      * @param player can be null in case button is released. Beware of that!
      * @return a new tagCompound or null if no change needed
      */
-    CompoundTag update(CompoundTag tagCompound, Level world, Player player);
+    ItemStack update(ItemStack module, Level world, Player player);
 
 }
