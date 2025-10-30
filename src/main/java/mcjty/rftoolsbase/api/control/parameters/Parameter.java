83b9dd27ed188ff010c3bc02704f412d376640e2
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2025-10-30 18:44:47 +0100

    p... p... porting

--- src/main/java/mcjty/rftoolsbase/api/control/parameters/Parameter.java
+++ src/main/java/mcjty/rftoolsbase/api/control/parameters/Parameter.java
@@ -11,6 +11,8 @@ import java.util.Objects;
  */
 public class Parameter implements IParameter {
 
+    public static final Parameter EMPTY = Parameter.builder().build();
+
     private final ParameterType parameterType;
     private final ParameterValue parameterValue;
 
