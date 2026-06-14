63a9e7b451c5621a731584e71fa9ce0512436f3c
McJty <mcjty1@gmail.com>
2026-06-14 08:34:33 +0200

    Various Patchouli documentation fixes and upates

--- src/main/java/mcjty/rftoolsbase/tools/ManualHelper.java
+++ src/main/java/mcjty/rftoolsbase/tools/ManualHelper.java
@@ -6,6 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 public class ManualHelper {
 
     public static ManualEntry create(String entryName) {
-        return new ManualEntry(new ResourceLocation("rftoolsbase:manual"), new ResourceLocation(entryName), 0);
+        ResourceLocation entry = new ResourceLocation(entryName);
+        return new ManualEntry(new ResourceLocation("rftoolsbase:manual"), new ResourceLocation("rftoolsbase", entry.getPath()), 0);
     }
 }
